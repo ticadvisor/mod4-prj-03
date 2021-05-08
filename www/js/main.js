545df638642a3634a99680df4e203034cc6c5ded
ticadvisor <javier.herrero@ticadvisor.com>
2021-05-08 10:59:38 +0200

    Initial version v.0.0

---
+++ www/js/main.js
@@ -0,0 +1,42 @@
+/**
+ * Completa la tabla de temperaturas tomando como referencia
+ * este array de ciudades. Tendrás que usar las siguientes
+ * clases para cambiar el color de fondo de cada temperatura.
+ *
+ *  - lower: temp. menor que 4: fondo azul.
+ *
+ *  - low: temp. entre 4 y 20: fondo verde
+ *
+ *  - medium: temp. entre 20 y 30: fondo naranja
+ *
+ *  - high: temp. mayor de 30: fondo rojo
+ *
+ */
+
+const temperaturas = [
+    {
+        city: 'A Coruña',
+        min: 17,
+        max: 23,
+    },
+    {
+        city: 'Ferrol',
+        min: 15,
+        max: 32,
+    },
+    {
+        city: 'Lugo',
+        min: -20,
+        max: 31,
+    },
+    {
+        city: 'Ourense',
+        min: 18,
+        max: 35,
+    },
+    {
+        city: 'Pontevedra',
+        min: 18,
+        max: 29,
+    },
+];
